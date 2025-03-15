a0a9c3422dc533b74cbbcffaf3ae05e17c206076
silvak <silvak.jeg@gmail.com>
2025-03-15 11:50:07 -0400

    Update main js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
 import "./index.css";
-//export { Title } from "./Title/title";
+
 export { Button } from "./Button/Button";
+export { Text } from "./Text/Text";
+export { Stack } from "./Stack/Stack";
