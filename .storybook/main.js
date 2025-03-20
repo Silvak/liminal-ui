75413290e9d751ea9ab844588140895678b7614f
silvak <silvak.jeg@gmail.com>
2025-03-20 15:07:37 -0400

    update package.json and remove storybook-static

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,5 @@
-
-
 /** @type { import('@storybook/react-vite').StorybookConfig } */
+
 const config = {
   "stories": [
     "../src/**/*.mdx",
@@ -12,9 +11,20 @@ const config = {
     "@chromatic-com/storybook",
     "@storybook/addon-interactions"
   ],
+  "core": {
+    "builder": "@storybook/builder-vite"
+  },
   "framework": {
     "name": "@storybook/react-vite",
     "options": {}
+  },
+  async viteFinal(config){
+    config.optimizeDeps ={
+      ... (config.optimizeDeps || {}),
+      exclude: ["vite-plugin-dts", "@tailwindcss/vite"]//exclude plugins
+    }
+
+    return config;
   }
 };
 export default config;
--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [react(), tailwindcss()],
   build: {
     lib: {
       entry: resolve(__dirname, "src/main.js"),
