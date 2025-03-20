9ab4955839154dea2573de282eaf61a16695b509
silvak <silvak.jeg@gmail.com>
2025-03-20 19:29:26 -0400

    fix(package): update package and workflows

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,30 +1,27 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 
 const config = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/addon-links",
   ],
-  "core": {
-    "builder": "@storybook/builder-vite"
+  core: {
+    builder: "@storybook/builder-vite",
   },
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
   },
-  async viteFinal(config){
-    config.optimizeDeps ={
-      ... (config.optimizeDeps || {}),
-      exclude: ["vite-plugin-dts", "@tailwindcss/vite"]//exclude plugins
-    }
-
-    return config;
-  }
+  //async viteFinal(config) {
+  //  config.optimizeDeps = {
+  //    ...(config.optimizeDeps || {}),
+  //    exclude: ["vite-plugin-dts"]
+  //  };
+  //  return config;
+  //}
 };
 export default config;
